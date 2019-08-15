84f4626ae36342ca07adb5a3a8d2b564aab2f31d
Alexander Mentyu <notuxius@gmail.com>
2019-08-15 17:25:37 +0300

    Updates to rename file

--- recursive pattern_file_folder_rename.py
+++ recursive pattern_file_folder_rename.py
@@ -7,15 +7,15 @@ for subdir, dirs, files in os.walk(base_dir):
     # if not os.path.isdir(os.path.join(base_dir, dir)):
     #     continue
 
-    # print(subdir)
+    # print(dirs)
 
     for dir in dirs:
-        pattern = r"\s{2,}"
-        replace = " "
-        new_name = re.sub(pattern, replace, dir)
+        pattern = r""
+        replace = ""
+        new_name = re.sub(pattern, replace, dir, flags=re.IGNORECASE)
         if os.path.join(base_dir, dir) != os.path.join(base_dir, new_name):
             print(os.path.join(base_dir, subdir, dir))
             print(os.path.join(base_dir, subdir, new_name))
             print("=================================")
 
-            os.rename(os.path.join(base_dir, subdir, dir), os.path.join(base_dir, new_name))
+            os.rename(os.path.join(base_dir, subdir, dir), os.path.join(base_dir, subdir, new_name))
