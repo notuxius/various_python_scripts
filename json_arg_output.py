e1dd6af6d96e8ef57685479978eb4f3edfa80bdd
Alexander Mentyu <notuxius@gmail.com>
2019-08-17 22:10:49 +0300

    Update, optimizations

--- json_arg_output.py
+++ json_arg_output.py
@@ -7,12 +7,29 @@ from sys import argv as sys_argv
 from re import sub as re_sub
 
 
-def print_error(error_text):
-    sys_stderr.write("ERROR: " + error_text + "\n")
+def print_error(error):
+    if isinstance(error, dict):
+        sys_stderr.write(str(error) + "\n")
+
+    else:
+        sys_stderr.write("ERROR: " + str(error) + "\n")
 
 
-def print_info(info_text):
-    print("INFO: " + info_text)
+def print_info(info):
+    if isinstance(info, dict):
+        print(str(info))
+
+    else:
+        print("INFO: " + str(info))
+
+# TODO Split this func into two
+def item_is_present_and_has_correct_type(item, item_type, input_json_object):
+    return bool(item in input_json_object and
+                isinstance(input_json_object[item], item_type) and input_json_object[item])
+
+
+def scheme_is_correct(input_scheme):
+    return bool(input_scheme in ("http", "https"))
 
 
 try:
@@ -38,129 +55,94 @@ except FileNotFoundError:
 for json_object in json_file_contents:
     aggr_uri = ""
 
-    if "disabled" in json_object:
-        if isinstance(json_object["disabled"], bool):
-            if json_object["disabled"]:
-                continue
+    if item_is_present_and_has_correct_type("disabled", bool, json_object):
+        continue
 
-            else:
-                print_info(
-                    "No need to explicitly set disabled value as false (all are enabled by default) for the domain:")
-
-        elif str(json_object["disabled"]).strip() == "":
-            print_info("Disabled value is empty for the domain:")
-
-        else:
-            print_error(
-                "Disabled value not defined as bool true or false or for the object:")
-            print_error(str(json_object))
-            print()
-            continue
-
-    if "domain_name" not in json_object or not json_object["domain_name"]:
-        print_error(
-            "Required domain name is not defined or is empty for the object:")
-        print_error(str(json_object))
+    if not item_is_present_and_has_correct_type("domain_name", str, json_object):
+        print_error("Required domain name is not defined or is empty:")
+        print_error(json_object)
         print()
         continue
 
-    if ("scheme" in json_object and not json_object["scheme"]) or not "scheme" in json_object:
-        print_info("Scheme is not defined for the domain, using http scheme:")
+    if item_is_present_and_has_correct_type("scheme", str, json_object):
+        scheme = json_object["scheme"]
+
+        if scheme_is_correct(scheme):
+            aggr_uri += scheme
+            aggr_uri += "://"
+
+    if not aggr_uri.startswith("http"):
+        print_info("Correct URI scheme is not defined, using HTTP:")
         aggr_uri += "http://"
 
-    elif json_object["scheme"] == "http" or json_object["scheme"] == "https":
-        aggr_uri += json_object["scheme"]
-        aggr_uri += "://"
+    if item_is_present_and_has_correct_type("username", str, json_object):
+        user_name = json_object["username"]
 
-    else:
-        print_error(
-            "Valid (http or https) URI scheme is not defined for the domain:")
+        if len(user_name) <= 255:
+            aggr_uri += user_name
 
-    if "username" in json_object and json_object["username"]:
-        if isinstance(json_object["username"], str) and len(json_object["username"]) <= 255:
-            aggr_uri += json_object["username"]
+            if item_is_present_and_has_correct_type("password", str, json_object):
+                password = json_object["password"]
 
-            if "password" in json_object and json_object["password"]:
-                if isinstance(json_object["password"], str) and len(json_object["password"]) <= 255:
+                if len(password) <= 255:
                     aggr_uri += ":"
-                    aggr_uri += json_object["password"]
+                    aggr_uri += password
 
                 else:
-                    print_error(
-                        "Password is not a string or is to long (255 characters limit) for the domain:")
+                    print_error("Password is to long (255 characters limit):")
 
             aggr_uri += "@"
 
         else:
-            print_error(
-                "User name is not a string or is to long (255 characters limit) for the domain:")
+            print_error("User name is to long (255 characters limit):")
 
     aggr_uri += json_object["domain_name"]
 
-    if "path" in json_object and json_object["path"]:
-        if not isinstance(json_object["path"], str):
-            print_error("Path is not a string for the domain:")
-            json_object["path"] = str(json_object["path"])
+    if item_is_present_and_has_correct_type("path", str, json_object):
+        path = json_object["path"]
 
-        if not json_object["path"].startswith("/"):
+        if not path.startswith("/"):
             aggr_uri += "/"
 
-        aggr_uri += json_object["path"]
+        aggr_uri += path
 
-        if "fragment" in json_object and json_object["fragment"]:
-            if not isinstance(json_object["fragment"], str):
-                print_error("Fragment is not a string for the domain:")
-                json_object["fragment"] = str(json_object["fragment"])
+        if item_is_present_and_has_correct_type("fragment", str, json_object):
+            fragment = json_object["fragment"]
 
-            if not json_object["fragment"].startswith("#"):
+            if not fragment.startswith("#"):
                 aggr_uri += "#"
 
-            aggr_uri += json_object["fragment"]
+            aggr_uri += fragment
 
-        if "query" in json_object and json_object["query"]:
-            for index, key in enumerate(json_object["query"]):
+        if item_is_present_and_has_correct_type("query", dict, json_object):
+            query = json_object["query"]
+
+            for index, key in enumerate(query):
+
                 if key:
+
                     if not "?" in aggr_uri:
                         aggr_uri += "?"
 
                     aggr_uri += key
 
-                    if json_object["query"][key]:
+                    if query[key]:
                         aggr_uri += "="
-
-                    if not isinstance(json_object["query"][key], str):
-                        print_error(
-                            "Value in query is not a string for the domain:")
-                        json_object["query"][key] = str(
-                            json_object["query"][key])
-
-                    aggr_uri += json_object["query"][key]
-                    aggr_uri += "&"
-
-                else:
-                    print_error("Key in query is empty for the domain:")
-                    continue
+                        aggr_uri += str(query[key])
+                        aggr_uri += "&"
 
             if aggr_uri.endswith("&"):
                 aggr_uri = re_sub("&$", "", aggr_uri)
 
-    else:
-        print_error(
-            "Not building possible remaining query because path is empty for the domain:")
+    if item_is_present_and_has_correct_type("port", int, json_object):
+        port = json_object["port"]
 
-    if "port" in json_object and json_object["port"]:
-        try:
-            if 1 <= json_object["port"] <= 65535:
-                aggr_uri += ":"
-                aggr_uri += str(json_object["port"])
+        if 1 <= port <= 65535:
+            aggr_uri += ":"
+            aggr_uri += port
 
-            else:
-                print_error(
-                    "Port is not in range from 1 to 65535 for the domain:")
-
-        except TypeError:
-            print_error(
-                "Port is not an integer for the domain:")
+        else:
+            print_error("Port is not in range from 1 to 65535:")
 
     print(aggr_uri)
     print()
