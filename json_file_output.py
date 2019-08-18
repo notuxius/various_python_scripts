e3a90e0bbada714d0af4f09a7959b9b1a63cece5
Alexander Mentyu <notuxius@gmail.com>
2019-08-18 13:09:44 +0300

    Validate  URL

--- json_file_output.py
+++ json_file_output.py
@@ -5,6 +5,9 @@ from sys import stderr as sys_stderr
 from sys import exit as sys_exit
 from sys import argv as sys_argv
 from re import sub as re_sub
+from re import compile as re_compile
+from re import match as re_match
+from re import IGNORECASE as re_ignore_case
 
 
 def print_error(error):
@@ -25,10 +28,6 @@ def print_info(info):
 
 def item_is_present(item, input_json_object):
     return bool(item in input_json_object and input_json_object[item])
-
-
-def scheme_is_correct(input_scheme):
-    return bool(input_scheme in ("http", "https"))
 
 
 try:
@@ -53,7 +52,7 @@ except FileNotFoundError:
 
 
 for json_object in json_file_contents:
-    aggr_uri = ""
+    aggr_URL = ""
 
     if item_is_present("disabled", json_object):
         disabled = json_object["disabled"]
@@ -71,26 +70,19 @@ for json_object in json_file_contents:
 
     if item_is_present("scheme", json_object):
         scheme = json_object["scheme"]
+        aggr_URL += scheme
+        aggr_URL += "://"
 
-        if scheme_is_correct(scheme):
-            aggr_uri += scheme
-            aggr_uri += "://"
-
-        else:
-            print_error("URI scheme is not HTTP or HTTPS")
-            print_error(json_object)
-            continue
-
-    # if not aggr_uri.startswith("http"):
-    #     print_info("Correct URI scheme is not defined, using HTTP")
-    #     aggr_uri += "http://"
+    else:
+        print_info("Correct URL scheme is not defined, using HTTP")
+        aggr_URL += "http://"
 
     if item_is_present("username", json_object):
         user_name = json_object["username"]
 
         try:
             if len(user_name) <= 255:
-                aggr_uri += user_name
+                aggr_URL += user_name
 
             else:
                 print_error("User name is to long (255 characters limit)")
@@ -105,8 +97,8 @@ for json_object in json_file_contents:
 
             try:
                 if len(password) <= 255:
-                    aggr_uri += ":"
-                    aggr_uri += password
+                    aggr_URL += ":"
+                    aggr_URL += password
 
                 else:
                     print_error("Password is to long (255 characters limit)")
@@ -116,10 +108,10 @@ for json_object in json_file_contents:
                 print_error(json_object)
                 continue
 
-        aggr_uri += "@"
+        aggr_URL += "@"
 
     try:
-        aggr_uri += domain_name
+        aggr_URL += domain_name
 
     except TypeError:
         print_error("Domain name is not a string")
@@ -131,9 +123,9 @@ for json_object in json_file_contents:
 
         try:
             if not path.startswith("/"):
-                aggr_uri += "/"
+                aggr_URL += "/"
 
-            aggr_uri += path
+            aggr_URL += path
 
         except (AttributeError, TypeError):
             print_error("Path is not a string")
@@ -145,9 +137,9 @@ for json_object in json_file_contents:
 
             try:
                 if not fragment.startswith("#"):
-                    aggr_uri += "#"
+                    aggr_URL += "#"
 
-                aggr_uri += fragment
+                aggr_URL += fragment
 
             except (AttributeError, TypeError):
                 print_error("Fragment is not a string")
@@ -160,26 +152,26 @@ for json_object in json_file_contents:
             for index, key in enumerate(query):
                 if key:
 
-                    if not "?" in aggr_uri:
-                        aggr_uri += "?"
+                    if not "?" in aggr_URL:
+                        aggr_URL += "?"
 
-                    aggr_uri += key
+                    aggr_URL += key
 
                     if query[key]:
-                        aggr_uri += "="
-                        aggr_uri += str(query[key])
-                        aggr_uri += "&"
+                        aggr_URL += "="
+                        aggr_URL += str(query[key])
+                        aggr_URL += "&"
 
-            if aggr_uri.endswith("&"):
-                aggr_uri = re_sub("&$", "", aggr_uri)
+            if aggr_URL.endswith("&"):
+                aggr_URL = re_sub("&$", "", aggr_URL)
 
     if item_is_present("port", json_object):
         port = json_object["port"]
 
         try:
             if 1 <= port <= 65535:
-                aggr_uri += ":"
-                aggr_uri += str(port)
+                aggr_URL += ":"
+                aggr_URL += str(port)
 
             else:
                 print_error("Port is not in range from 1 to 65535")
@@ -187,5 +179,20 @@ for json_object in json_file_contents:
         except TypeError:
             print_error("Port is not an integer")
 
-    print(aggr_uri)
-    print()
+    pattern = re_compile(
+        r'^https?://' # http:// or https://
+        r'(?:[A-Z0-9]{1,255}:)?(?:[A-Z0-9]{1,255}@)?' # User name and optional password
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' # Domain...
+        r'localhost|' # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or IP
+        r'(?::\d+)?' # Optional port
+        r'(?:/?|[/?]\S+)$', re_ignore_case)
+
+    if re_match(pattern, aggr_URL):
+        print(aggr_URL)
+        print()
+
+    else:
+        print_error("Not valid URL")
+        print_error(json_object)
+        # print_error(aggr_URL)
